bf1a9b0583fbe3e86aef1700f83957eceff3478d
Gustavo-silva05 <149391594+Gustavo-silva05@users.noreply.github.com>
2024-04-27 00:09:37 -0300

    Add files via upload

--- Timer.v
+++ Timer.v
@@ -1,26 +1,47 @@
 module Timer(
     input clock,
     input reset,
-    input [3:0] value,
+    input [4:0] value,
     input start_timer,
     output expired,
-    output one_hz_enable,
-    output half_haz_enable;
-)
-reg [3:0] count;
+    output one_hz_enable
+);
+reg [4:0] count;
+reg [27:0] timer_1sec;
+reg expirou;
+reg one_hz;
 
-always @(posedge clk ) begin
+always @(posedge clock ) begin
     if (reset) begin
+        timer_1sec <= 28'd100_000_000;
+        expirou = 1'b0;
+    end
+    else begin
         if (start_timer) begin
             count <= value;
         end
-    end
-    else begin
-        if (one_hz_enable) begin
-            count <= count - 4'd1;
+        else if (timer_1sec > 28'd0) begin
+            timer_1sec <= timer_1sec - 28'd1;
+        end
+        else begin
+            one_hz = 1'b1;
         end
     end
 end
 
+always @* begin
+    if ( count == 5'd0 ) begin
+        expirou = 1'b1;
+    end
+    else if (one_hz_enable) begin
+        count <= count - 5'd1;
+        timer_1sec <= 28'd100_000_000;
+        one_hz = 1'b0;
+    end
+end
+
+assign expired = expirou;
+assign one_hz_enable = one_hz;
 
 endmodule
+
