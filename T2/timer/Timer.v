b9561ad8cf2630b7de4b98c686eb430ff57c2ee9
Gustavo Souza da Silva <149391594+Gustavo-silva05@users.noreply.github.com>
2024-06-12 09:14:37 -0300

    Update Timer.v

--- T2/timer/Timer.v
+++ T2/timer/Timer.v
@@ -4,45 +4,45 @@ module Timer(
     input [4:0] value,
     input start_timer,
     output expired,
-    output one_hz_enable
+    output one_hz_enable, two_hz_enable
 );
 reg [4:0] count;
-reg [27:0] timer_1sec;
+reg [28:0] timer_2sec;
 reg expirou;
-reg one_hz;
+reg one_hz, two_hz;
 
 always @(posedge clock ) begin
     if (reset) begin
-        timer_1sec <= 28'd100_000_000;
+        timer_2sec <= 29'd200_000_000;
         expirou = 1'b0;
     end
     else begin
         if (start_timer) begin
-            count <= value;
             expirou = 1'b0;
-        end
-        else if (timer_1sec > 28'd0) begin
-            timer_1sec <= timer_1sec - 28'd1;
+            count <= value;
         end
         else begin
-            one_hz = 1'b1;
+            if (count > 5'd0) begin
+                if (timer_2sec > 29'd0) begin
+                    if (timer_2sec == 29'd100_000_000) begin
+                        one_hz <= 1'b1;
+                    end
+                    else begin
+                        timer_2sec <= timer_2sec - 29'd1;
+                        one_hz <= 1'b0;
+                    end
+                end
+                else begin
+                    two_hz <= 1'b1;
+                end
+            end
         end
-    end
-end
-
-always @* begin
-    if ( count == 5'd0 ) begin
-        expirou = 1'b1;
-    end
-    else if (one_hz_enable) begin
-        count <= count - 5'd1;
-        timer_1sec <= 28'd100_000_000;
-        one_hz = 1'b0;
     end
 end
 
 assign expired = expirou;
 assign one_hz_enable = one_hz;
+assign two_hz_enable = two_hz;
 
 endmodule
 
