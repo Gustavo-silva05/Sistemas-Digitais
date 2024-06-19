2cda462243cff6fc2c46f4ef3017cc2ffe6ebc89
Gustavo Souza da Silva <149391594+Gustavo-silva05@users.noreply.github.com>
2024-06-19 09:02:02 -0300

    Update Parametros_Tempo.v

--- T2/Parametros_Tempo/Parametros_Tempo.v
+++ T2/Parametros_Tempo/Parametros_Tempo.v
@@ -21,23 +21,25 @@ always @(posedge clock) begin
         T_PASSAGER_DELAY <= 4'b1111;
         T_ALARM_ON <= 4'b1010;
     end
-    case (interval)
-        2'b00: valor <= T_ARM_DELAY;
-        2'b01: valor <= T_DRIVER_DELAY;
-        2'b10: valor <= T_PASSAGER_DELAY;
-        2'b11: valor <= T_ALARM_ON;
-    endcase
-end
-
-always @* begin
-    if (reprogram) begin
+    else begin
+        if (reprogram) begin
         case (time_param_sel)
             2'b00: T_ARM_DELAY <= time_value;
             2'b01: T_DRIVER_DELAY <= time_value;
             2'b10: T_PASSAGER_DELAY <= time_value;
             2'b11: T_ALARM_ON <= time_value;
         endcase 
+        end
     end
+end
+
+always @* begin
+    case (interval)
+        2'b00: valor <= T_ARM_DELAY;
+        2'b01: valor <= T_DRIVER_DELAY;
+        2'b10: valor <= T_PASSAGER_DELAY;
+        2'b11: valor <= T_ALARM_ON;
+    endcase
 end
 
 assign value = valor;
