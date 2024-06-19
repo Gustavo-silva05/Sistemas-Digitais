ed75640e43f3a89dafb115089b18cc11a55cf606
Gustavo-silva05 <gus.ssilva05@gmail.com>
2024-06-19 14:24:03 -0300

    t2

--- T2/timer/Timer.v
+++ T2/timer/Timer.v
@@ -14,7 +14,7 @@ reg one_hz, two_hz;
 
 always @(posedge clock ) begin
     if (reset) begin
-        timer_2sec <= 29'd200_0;
+        timer_2sec <= 29'd200_000_000;
         expirou = 1'b0;
         count <= 4'd1;
     end
@@ -22,7 +22,7 @@ always @(posedge clock ) begin
         if (start_timer) begin
             expirou = 1'b0;
             count <= value;
-            timer_2sec <= 29'd200_0;
+            timer_2sec <= 29'd200_000_000;
         end
         else begin
             if (count > 4'd0) begin
@@ -30,7 +30,7 @@ always @(posedge clock ) begin
                 if (timer_2sec > 29'd0) begin
                     two_hz <= 1'b0;
                     one_hz <= 1'b0;
-                    if (timer_2sec == 29'd100_0) begin
+                    if (timer_2sec == 29'd100_000_000) begin
                         one_hz <= 1'b1;
                         count <= count - 5'd1;
                         timer_2sec <= timer_2sec - 29'd1;
@@ -41,7 +41,7 @@ always @(posedge clock ) begin
                     end
                 end
                 else begin
-                    timer_2sec <= 29'd200_0;
+                    timer_2sec <= 29'd200_000_000;
                     two_hz <= 1'b1;
                     one_hz <= 1'b1;
                     count <= count - 5'd1;
