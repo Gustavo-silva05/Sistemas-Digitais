9e0e6ef8ca70ee052068da35cad2439a65b28ddc
Gustavo Souza <gus.ssilva05@gmail.com>
2024-07-04 21:53:49 -0300

    .

--- T3/top.v
+++ T3/top.v
@@ -1,6 +1,19 @@
 module top 
 (
-  // Declarar os pinos de IO
+  input clk, rst, start_f, start_t, stop_f_t, opdate,
+  input [2:0] prog,
+  output [5:0] led, 
+  output parity,
+  output [7:0] an, dec_dpp
 );
+
+wire f_valid;
+wire [15:0] f_out;
+
+wire t_valid;
+wire t_out;
+
+wire [2:0] prog_out;
+wire clk_2, clk_1;
 
 endmodule
