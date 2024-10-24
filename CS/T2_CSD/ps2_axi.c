25ab3fc651b1137156a072aed34f7f130e359cab
Gustavo Souza da Silva <149391594+Gustavo-silva05@users.noreply.github.com>
2024-10-24 15:08:57 -0300

    .

--- CS/T2_CSD/ps2_axi.c
+++ CS/T2_CSD/ps2_axi.c
@@ -1,60 +1,25 @@
 #include <hf-risc.h>
 
-#define PS2_AXI_BASE         0xe4a90000
-#define PS2_AXI_STATUS       (*(volatile uint32_t *)(PS2_AXI_BASE + 0x010))
-#define PS2_AXI_DATA         (*(volatile uint32_t *)(PS2_AXI_BASE + 0x020))
+#define PS2_AXI_BASE 0xe4a90000
+#define PS2_AXI_STATUS (*(volatile uint32_t *)(PS2_AXI_BASE + 0x010))
+#define PS2_AXI_DATA (*(volatile uint32_t *)(PS2_AXI_BASE + 0x020))
 
-#define PS2_STREADY          (1 << 0)
-#define PS2_STVALID          (1 << 1)
+#define PS2_STREADY (1 << 0)
+#define PS2_STVALID (1 << 1)
 
-uint8_t read_ps2_data() {
-    uint8_t data;
+uint8_t read_ps2_data()
+{
     while (!(PS2_AXI_STATUS & PS2_STVALID)); // Espera até o dado estar disponível
-    data = PS2_AXI_DATA;
-    return data;
+    return PS2_AXI_DATA;
 }
 
-int main(void) {
+int main(void)
+{
     printf("Iniciando leitura do teclado...\n");
-
-    while (1) {
-        uint8_t tecla = read_ps2_data();
-        printf("Tecla pressionada: %02x\n", tecla);
-        delay_ms(1000);
+    while (1)
+    {
+        printf("Tecla pressionada: %02X\n", read_ps2_data());
     }
 
     return 0;
 }
-
-// #include <hf-risc.h>
-
-// #define SW_AXI_BASE			    0xe4a90000
-// #define SW_AXI_STATUS			(*(volatile uint32_t *)(SW_AXI_BASE + 0x010))
-// #define SW_AXI_SDATA			(*(volatile uint32_t *)(SW_AXI_BASE + 0x020))
-
-// #define SW_AXI_STREADY			(1 << 0)
-// #define SW_AXI_STVALID			(1 << 1)
-
-
-
-// uint8_t sw_axi()
-// {
-// 	uint8_t data;
-	
-// 	data = SW_AXI_SDATA;
-// 	while (!(SW_AXI_STATUS & SW_AXI_STVALID));
-// 	data = SW_AXI_SDATA;
-	
-// 	return data;
-// }
-
-// int main(void){
-// 	printf("hello\n");
-	
-// 	while (1) {
-// 		printf("sw val: %02x\n", sw_axi());
-// 		delay_ms(1000);		
-// 	}
-
-// 	return 0;
-// }
