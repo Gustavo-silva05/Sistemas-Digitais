d6d06af440480279dab4c94e8769726cd0c530df
Gustavo Souza da Silva <149391594+Gustavo-silva05@users.noreply.github.com>
2024-10-24 12:57:11 -0300

    .

--- CS/T2_CSD/ps2_axi.c
+++ CS/T2_CSD/ps2_axi.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <hf-risc.h>
 
 #define PS2_AXI_BASE         0xe4a90000
@@ -9,8 +8,10 @@
 #define PS2_STVALID          (1 << 1)
 
 uint8_t read_ps2_data() {
+    uint8_t data;
     while (!(PS2_AXI_STATUS & PS2_STVALID)); // Espera até o dado estar disponível
-    return PS2_AXI_DATA;
+    data = PS2_AXI_DATA;
+    return data;
 }
 
 int main(void) {
@@ -19,7 +20,41 @@ int main(void) {
     while (1) {
         uint8_t tecla = read_ps2_data();
         printf("Tecla pressionada: %02x\n", tecla);
+        delay_ms(1000);
     }
 
     return 0;
 }
+
+// #include <hf-risc.h>
+
+// #define SW_AXI_BASE			    0xe4a90000
+// #define SW_AXI_STATUS			(*(volatile uint32_t *)(SW_AXI_BASE + 0x010))
+// #define SW_AXI_SDATA			(*(volatile uint32_t *)(SW_AXI_BASE + 0x020))
+
+// #define SW_AXI_STREADY			(1 << 0)
+// #define SW_AXI_STVALID			(1 << 1)
+
+
+
+// uint8_t sw_axi()
+// {
+// 	uint8_t data;
+	
+// 	data = SW_AXI_SDATA;
+// 	while (!(SW_AXI_STATUS & SW_AXI_STVALID));
+// 	data = SW_AXI_SDATA;
+	
+// 	return data;
+// }
+
+// int main(void){
+// 	printf("hello\n");
+	
+// 	while (1) {
+// 		printf("sw val: %02x\n", sw_axi());
+// 		delay_ms(1000);		
+// 	}
+
+// 	return 0;
+// }
